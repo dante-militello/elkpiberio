c15de0c963118967c39688510ba1dbf5c464801b
Dan <36249892+dante-militello@users.noreply.github.com>
2024-02-16 00:35:35 -0300

    Removing the @domain concept on the "other user" profile 3 dots menu

--- components/account/AccountMoreButton.vue
+++ components/account/AccountMoreButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { mastodon } from 'masto'
-import { toggleBlockAccount, toggleBlockDomain, toggleMuteAccount } from '~~/composables/masto/relationship'
+import { toggleBlockAccount, toggleMuteAccount } from '~~/composables/masto/relationship'
 
 const { account } = defineProps<{
   account: mastodon.v1.Account
@@ -27,7 +27,7 @@ function shareAccount() {
 async function toggleReblogs() {
   if (!relationship!.showingReblogs && await openConfirmDialog({
     title: t('confirm.show_reblogs.title'),
-    description: t('confirm.show_reblogs.description', [account.acct]),
+    description: t('confirm.show_reblogs.description', [account.displayName]),
     confirm: t('confirm.show_reblogs.confirm'),
     cancel: t('confirm.show_reblogs.cancel'),
   }) !== 'confirm')
@@ -71,13 +71,13 @@ async function removeUserNote() {
       <template v-if="currentUser">
         <template v-if="!isSelf">
           <CommonDropdownItem
-            :text="$t('menu.mention_account', [`@${account.acct}`])"
+            :text="$t('menu.mention_account', [`@${account.displayName}`])"
             icon="i-ri:at-line"
             :command="command"
             @click="mentionUser(account)"
           />
           <CommonDropdownItem
-            :text="$t('menu.direct_message_account', [`@${account.acct}`])"
+            :text="$t('menu.direct_message_account', [`@${account.displayName}`])"
             icon="i-ri:message-3-line"
             :command="command"
             @click="directMessageUser(account)"
@@ -86,13 +86,13 @@ async function removeUserNote() {
           <CommonDropdownItem
             v-if="!relationship?.showingReblogs"
             icon="i-ri:repeat-line"
-            :text="$t('menu.show_reblogs', [`@${account.acct}`])"
+            :text="$t('menu.show_reblogs', [`@${account.displayName}`])"
             :command="command"
             @click="toggleReblogs()"
           />
           <CommonDropdownItem
             v-else
-            :text="$t('menu.hide_reblogs', [`@${account.acct}`])"
+            :text="$t('menu.hide_reblogs', [`@${account.displayName}`])"
             icon="i-ri:repeat-line"
             :command="command"
             @click="toggleReblogs()"
@@ -100,14 +100,14 @@ async function removeUserNote() {
 
           <CommonDropdownItem
             v-if="!relationship?.note || relationship?.note?.length === 0"
-            :text="$t('menu.add_personal_note', [`@${account.acct}`])"
+            :text="$t('menu.add_personal_note', [`@${account.displayName}`])"
             icon="i-ri-edit-2-line"
             :command="command"
             @click="addUserNote()"
           />
           <CommonDropdownItem
             v-else
-            :text="$t('menu.remove_personal_note', [`@${account.acct}`])"
+            :text="$t('menu.remove_personal_note', [`@${account.displayName}`])"
             icon="i-ri-edit-2-line"
             :command="command"
             @click="removeUserNote()"
@@ -115,14 +115,14 @@ async function removeUserNote() {
 
           <CommonDropdownItem
             v-if="!relationship?.muting"
-            :text="$t('menu.mute_account', [`@${account.acct}`])"
+            :text="$t('menu.mute_account', [`@${account.displayName}`])"
             icon="i-ri:volume-mute-line"
             :command="command"
             @click="toggleMuteAccount (relationship!, account)"
           />
           <CommonDropdownItem
             v-else
-            :text="$t('menu.unmute_account', [`@${account.acct}`])"
+            :text="$t('menu.unmute_account', [`@${account.displayName}`])"
             icon="i-ri:volume-up-fill"
             :command="command"
             @click="toggleMuteAccount (relationship!, account)"
@@ -130,38 +130,21 @@ async function removeUserNote() {
 
           <CommonDropdownItem
             v-if="!relationship?.blocking"
-            :text="$t('menu.block_account', [`@${account.acct}`])"
+            :text="$t('menu.block_account', [`@${account.displayName}`])"
             icon="i-ri:forbid-2-line"
             :command="command"
             @click="toggleBlockAccount (relationship!, account)"
           />
           <CommonDropdownItem
             v-else
-            :text="$t('menu.unblock_account', [`@${account.acct}`])"
+            :text="$t('menu.unblock_account', [`@${account.displayName}`])"
             icon="i-ri:checkbox-circle-line"
             :command="command"
             @click="toggleBlockAccount (relationship!, account)"
           />
 
-          <template v-if="getServerName(account) !== currentServer">
-            <CommonDropdownItem
-              v-if="!relationship?.domainBlocking"
-              :text="$t('menu.block_domain', [getServerName(account)])"
-              icon="i-ri:shut-down-line"
-              :command="command"
-              @click="toggleBlockDomain(relationship!, account)"
-            />
-            <CommonDropdownItem
-              v-else
-              :text="$t('menu.unblock_domain', [getServerName(account)])"
-              icon="i-ri:restart-line"
-              :command="command"
-              @click="toggleBlockDomain(relationship!, account)"
-            />
-          </template>
-
           <CommonDropdownItem
-            :text="$t('menu.report_account', [`@${account.acct}`])"
+            :text="$t('menu.report_account', [`@${account.displayName}`])"
             icon="i-ri:flag-2-line"
             :command="command"
             @click="openReportDialog(account)"
